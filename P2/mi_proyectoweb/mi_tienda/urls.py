1e560d56c78cf44ff719b148bb9e81dcd8e2d06e
nievescanas <nievescanasmartin@gmail.com>
2020-03-10 19:29:12 +0100

    introducción a páginas dinámicas

--- P2/mi_proyectoweb/mi_tienda/urls.py
+++ P2/mi_proyectoweb/mi_tienda/urls.py
@@ -9,4 +9,9 @@ from . import views
 urlpatterns = [
     # -- Vista pricipal (índice)
     path('', views.index, name='index'),
+    path('test1/', views.test1, name='test1'),
+    path('test2/', views.test2, name='test2'),
+    path('test3/', views.test3, name='test3'),
+    path('test4/', views.test4, name='test4'),
+    path('test5/', views.test5, name='test5'),
 ]
--- P2/mi_proyectoweb/mi_tienda/views.py
+++ P2/mi_proyectoweb/mi_tienda/views.py
@@ -1,11 +1,100 @@
 from django.shortcuts import render
 
+from django.template import Template, Context
+from django.template.loader import get_template
 # Create your views here.
 # -- Fichero mi_tienda/views.py
 from django.http import HttpResponse
+from random import randint
+
 
 # -- Vista principal de mi tienda
 # -- El nombre de la vista puede ser cualquiera. Nosotros lo hemos
 # -- llamado index, pero se podría haber llamado pepito
 def index(request):
     return HttpResponse("Hola! esta es la página principal de Mi tienda!")
+
+# -- Ejemplo de generacion a partir de cadenas con código html
+def test1(request):
+
+    # -- Obtener el número aleatorio
+    numero = randint(0, 100)
+
+    # Párrafo a insertar
+    P = "<p>Numero aleatorio: " + str(numero) + " </p>"
+
+    PAGINA_INI = """
+    <!DOCTYPE html>
+    <html lang="es" dir="ltr">
+      <head>
+        <meta charset="utf-8">
+        <title>Test1</title>
+      </head>
+      <body>
+        <h1>TEST1</h1>
+    """
+
+    PAGINA_FIN = """
+      </body>
+    </html>
+    """
+    return HttpResponse(PAGINA_INI + P + PAGINA_FIN)
+
+    # -- Ejemplo de generacion mediante una plantilla en el código
+def test2(request):
+
+    # -- Obtener el número aleatorio
+    numero = randint(0, 100)
+
+    PLANTILLA = """
+    <!DOCTYPE html>
+    <html lang="es" dir="ltr">
+      <head>
+        <meta charset="utf-8">
+        <title>Test2</title>
+      </head>
+      <body>
+        <h1>TEST2</h1>
+        <p> Numero aleatorio:  {{numero}} </p>
+      </body>
+    </html>
+    """
+
+    # --Procesar la plantilla
+    t = Template(PLANTILLA)
+
+    # -- Crear el contexto: Asignar el numero
+    c = Context({'numero': str(numero)})
+
+    # -- Obtener la pagina html final
+    html = t.render(c)
+
+    return HttpResponse(html)
+
+    # -- Ejemplo de generacion mediante una plantilla en FICHERO
+def test3(request):
+
+    # -- Obtener el número aleatorio
+    numero = randint(0, 100)
+
+    # -- Leer la plantilla del fichero
+    t = get_template('test.html')
+
+    # -- Crear el contexto: Asignar el numero
+    c = {'numero': str(numero)}
+
+    # -- Obtener la pagina html final
+    html = t.render(c)
+
+    return HttpResponse(html)
+
+    # -- Ejemplo de uso de la función Render (Compacto)
+def test4(request):
+    # -- Obtener el número aleatorio
+    numero = randint(0, 100)
+    return render(request, 'test.html', {'numero':str(numero)})
+
+def test5(request):
+    # -- Obtener el número aleatorio
+    numero = randint(0, 100)
+    return render(request, 'test5.html', {'numero':str(numero)})
