e86de9aeea384d4bf03a0232c9ce91cfe516bc6a
nievescanas <nievescanasmartin@gmail.com>
2020-07-12 17:03:40 +0200

    Mandar cookie cuando pulsa registro

---
+++ P3/server.js
@@ -0,0 +1,84 @@
+//-- Puerto donde recibir las peticiones
+const PUERTO = 8080;
+
+//-- Modulo http
+const http = require('http');
+var url = require('url');
+
+//-- Acceso al módulo fs, para lectura de ficheros
+var fs = require('fs');
+var filename
+
+//-- Recursos para la ejecución ls
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+console.log("Arrancando servidor...")
+
+//-- Inicializar el servidor cada vez que recibe una petición
+//-- Funcion para atender solo a una Peticion
+//-- req: Mensaje de solicitud
+//-- res: Mensaje de respuesta
+http.createServer((req, res) => {
+  console.log("---> Peticion recibida")
+  console.log("Recurso solicitado (URL): " + req.url)
+
+  //-- Partes de la url
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  let qdata = q.query;
+  //-- Leer las cookies
+  const cookie = req.headers.cookie;
+  console.log("Cookie: " + cookie)
+
+  //-- Información que nos proporciona
+  console.log("Pathname: " +  q.pathname)
+  console.log("search: " + q.search)
+  console.log("Búsqueda:")
+  console.log(qdata)
+  console.log("Artículo: " + qdata.articulo)
+  console.log("Color: " + qdata.color)
+
+
+  //-- Pagina principal y Tipo mime por petición
+  let mime = "text/"
+  let mime_img = "image/"
+
+
+  if (filename == "./"){
+    filename = "./page_structure.html"
+    mime = mime + "html"
+  }else if (filename == "./registry.html"){
+    res.setHeader('Set-Cookie', 'user=obijuan')
+    mime = mime + "html"
+  }else{
+    let point_position = q.pathname.lastIndexOf(".")
+    let tipo = q.pathname.slice(point_position+1)
+
+    if (point_position == -1){
+      mime = mime + "html"
+      filename = filename + "/index.html"
+    }else if (tipo == "PNG") {
+      mime = mime_img + tipo
+    }else{
+      mime = mime + tipo
+    }
+  };
+
+
+//-- Leer fichero y construcción de respuesta
+fs.readFile(filename, function(err, data) {
+  if (err) {              //-- Control de error de lectura
+    res.writeHead(404, {'Content-Type': mime});
+    return res.end("404 Not Found");
+  }else{                //-- Contrucción menjase
+    res.writeHead(200, {'Content-Type': mime});
+    res.write(data);
+    return res.end();
+  }
+  console.log(res);
+});}).listen(PUERTO);
+
+
+console.log("Servidor LISTO!")
+console.log("Escuchando en puerto: " + PUERTO)
