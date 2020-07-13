74799c58d32a564de3abaca1a00ff99019308840
nievescanas <nievescanasmartin@gmail.com>
2020-07-13 12:21:00 +0200

    Cambio de if por case

--- P3/server.js
+++ P3/server.js
@@ -19,7 +19,7 @@ console.log("Arrancando servidor...")
 //-- Funcion para atender solo a una Peticion
 //-- req: Mensaje de solicitud
 //-- res: Mensaje de respuesta
-http.createServer((req, res) => {
+function peticion(req, res) {
   console.log("---> Peticion recibida")
   console.log("Recurso solicitado (URL): " + req.url)
 
@@ -27,58 +27,70 @@ http.createServer((req, res) => {
   var q = url.parse(req.url, true);
   var filename = "." + q.pathname;
   let qdata = q.query;
-  //-- Leer las cookies
   const cookie = req.headers.cookie;
-  console.log("Cookie: " + cookie)
-
-  //-- Información que nos proporciona
-  console.log("Pathname: " +  q.pathname)
-  console.log("search: " + q.search)
-  console.log("Búsqueda:")
-  console.log(qdata)
-  console.log("Artículo: " + qdata.articulo)
-  console.log("Color: " + qdata.color)
-
 
   //-- Pagina principal y Tipo mime por petición
   let mime = "text/"
   let mime_img = "image/"
 
-
-  if (filename == "./"){
-    filename = "./page_structure.html"
-    mime = mime + "html"
-  }else if (filename == "./registry.html"){
-    res.setHeader('Set-Cookie', 'user=obijuan')
-    mime = mime + "html"
-  }else{
-    let point_position = q.pathname.lastIndexOf(".")
-    let tipo = q.pathname.slice(point_position+1)
-
-    if (point_position == -1){
+  switch (q.pathname) {
+    case "/":
+      content = fs.readFileSync("./page_structure.html", "utf-8")
       mime = mime + "html"
-      filename = filename + "/index.html"
-    }else if (tipo == "PNG") {
-      mime = mime_img + tipo
-    }else{
-      mime = mime + tipo
-    }
-  };
+      res.statusCode = 200;
+      res.setHeader('Content-Type', mime)
+      res.write(content);
+      res.end();
+    break;
+    case "./registry.html":
+      res.setHeader('Set-Cookie', 'user=obijuan')
+      mime = mime + "html"
+      res.setHeader('Content-Type', mime)
+      res.write(content);
+      res.end();
+    break;
+    default:
+      let point_position = q.pathname.lastIndexOf(".")
+      let tipo = q.pathname.slice(point_position+1)
 
 
-//-- Leer fichero y construcción de respuesta
-fs.readFile(filename, function(err, data) {
-  if (err) {              //-- Control de error de lectura
-    res.writeHead(404, {'Content-Type': mime});
-    return res.end("404 Not Found");
-  }else{                //-- Contrucción menjase
-    res.writeHead(200, {'Content-Type': mime});
-    res.write(data);
-    return res.end();
-  }
-  console.log(res);
-});}).listen(PUERTO);
+      if (point_position == -1){
+        mime = mime + "html"
+        filename = filename + "/index.html"
+      }else if (tipo == "PNG") {
+        mime = mime_img + tipo
+      }else{
+        mime = mime + tipo
+      }
+
+      fs.readFile(filename, function(err, data) {
+          if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+          }
+          //-- Generar el mensaje de respuesta
+          res.writeHead(200, {'Content-Type': mime});
+          res.write(data);
+          res.end();
+        });
+        console.log(filename)
+      break
+
+   }
+
+   //-- Generar el mensaje de respuesta
 
 
-console.log("Servidor LISTO!")
-console.log("Escuchando en puerto: " + PUERTO)
+ }
+
+ //-- Inicializar el servidor
+ //-- Cada vez que recibe una petición
+ //-- invoca a la funcion peticion para atenderla
+ const server = http.createServer(peticion)
+
+ //-- Configurar el servidor para escuchar en el
+ //-- puerto establecido
+ server.listen(PUERTO);
+
+ console.log("Servidor LISTO!")
+ console.log("Escuchando en puerto: " + PUERTO)
